a13b79ed7918264f0a5e90b69511a8183d565c67
Benjamin Villa <ywqqqq@gmail.com>
2020-05-17 21:11:45 +0200

    _

--- doc/TODO.md
+++ doc/TODO.md
@@ -1,9 +1,9 @@
 # TODO 
-### Gab
+
 Person.Collision() : Gérer la reduction du risque de propagation si la personne porte un masque  
 
 Display : Faire des fonctions Render(), Update() etc pour séparer la logique et le rendu dans la boucle principale 
 
-Behavior : Ajouter des types de comportements au dict.  
+Behavior : Ajouter des types de comportements au dict.
 
-### Ben
+
--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import time
 import os
 from src.person import Person, generate
 
-#Pour gabriel qui utilise la magie noire afin de lancer les fenêtres sous windows#
-# try:
-#     os.environ["DISPLAY"]
-# except:
-#     os.environ["SDL_VIDEODRIVER"] = "dummy" 
-##################################################################################
-
 color = {
 	"WHITE" : [255,255,255],
 	"BLACK" : [0,0,0],
@@ -28,14 +21,14 @@ class Display:
 
 simulationWindow = Display()
 menuWindow = Display()
-currentWindow = interface[0]
+currentWindow = interface[1]
 population = 20
 PersonList,HitboxList = generate(simulationWindow.window,population)
 PersonList[0].infection(1, "Virus Presets/Covid.json")
 
 def menuRendering():
 	framerate = menuWindow.clock.tick(60)
-	simulationWindow.window.fill(color["BLACK"])
+	menuWindow.window.fill(color["BLACK"])
 
 def personRendering():
 	framerate = simulationWindow.clock.tick(60)
@@ -49,9 +42,9 @@ def personRendering():
 while 1 :
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: 
-			#sys.exit()
-			if currentWindow == interface[0] : currentWindow = interface[1]
-			elif currentWindow == interface[1] : currentWindow = interface[0]
+			sys.exit()
+			#if currentWindow == interface[0] : currentWindow = interface[1]
+			#elif currentWindow == interface[1] : currentWindow = interface[0]
 	if currentWindow == "menu" : menuRendering()
 	elif currentWindow == "simulation" : personRendering()
 	pygame.display.flip()
